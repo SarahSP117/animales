a9b77f53c80a5e7b08e2c13ecfe1f91256903af1
SaraSP117 <sarahevgm@gmail.com>
2025-06-16 07:46:32 -0600

    mi comentario

--- src/data/mamiferos.js
+++ src/data/mamiferos.js
@@ -2,18 +2,18 @@ export const mamifero = [
 
     {
         "id": "m1",
-        "nombre": "Okapi",
-        "nombre_cientifico":"Okapia johnstoni",
-        "orden":"",
-        "familia":"",
-        "descripcion":"",
-        "estado_conservacion":"",
-        "distribuicion":"",
-        "tamano":"",
-        "nutricion":"",
-        "habitad":"",
-        "esperanza_vida":"",
-        "curiosidades":["", "", "", "", "", ""]
+        "nombre": "Okapi.",
+        "nombre_cientifico":"Okapia johnstoni.",
+        "orden":"Artiodactyla..",
+        "familia":"Giraffidae.",
+        "descripcion":"El okapi es un animal de la familia de los mamíferos. Físicamente, parece un cruce entre una cebra, un caballo y una jirafa, aunque con el cuello y las patas mucho más cortas.",
+        "estado_conservacion":"En peligro de extinción.",
+        "distribuicion":"África subsahariana, cordillera de Etiopía.",
+        "tamano":"1,9 – 2,5 m.",
+        "nutricion":" se alimenta principalmente de hojas, brotes, ramas, frutos y hongos de diversas especies vegetales.",
+        "habitad":"El principal hábitad del Okapi es la Selva tropical de Ituri",
+        "esperanza_vida":"Entre 15 y 30 años",
+        "curiosidades":["El okapi es el único animal que puede lamerse las orejas.", "consumien hasta 27 kilos de vegetación al día.", "El principal depredador del okapi es el leopardo", "Los okapis emiten sonidos de baja frecuencia que escapan al oído humano.", "No hubo imágenes de un okapi en estado salvaje hasta 2008, cuando se capturaron las primeras fotografías con una cámara trampa.", "puede llegar a pesar unos 250 kilogramos."]
 
     },
 
